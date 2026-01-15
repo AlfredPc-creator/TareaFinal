4ed794715514e92b51a38b4fa3209dea10564f33
AlfredPc-creator <alfredprincipe@gmail.com>
2026-01-15 12:03:43 -0500

    Crear db

---
+++ database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "tarea01")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "users")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+users_collection = db[COLLECTION_NAME]
+
+
+users_collection.create_index("dni", unique=True)
